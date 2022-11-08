28f908b86fba2559f176a4938e37408d86be4ca9
Gaurang Dave <gaurangvdave@gmail.com>
2022-11-08 15:34:16 -0800

    intermediate commit - successully scrapped news articles from foxnews

--- gpu_test.py
+++ gpu_test.py
@@ -1,3 +1,7 @@
 import tensorflow as tf
+from tensorflow.python.client import device_lib
 
+print("devices: ", [d.name for d in device_lib.list_local_devices()])
+print("GPUs:    ", tf.config.list_physical_devices('GPU'))
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+print("TF v.:   ", tf.__version__)
